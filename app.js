dbafe06499964b210e790452ee149d8e058b24e2
George H. Thomas <georgebushdontcare@gmail.com>
2025-07-11 20:29:29 -0400

    dev

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import routes from "./routes/router.js";
 import * as db from "./config/db.js";
 
-// import { scrapeNewKCNA } from "./src/scrape-control.js";
+import { scrapeKCNAWatch } from "./src/scrape-control.js";
 
 //FIRST CONNECT TO DB
 // (need this HERE bc main function will execute before express and fuck everything)
@@ -19,6 +19,9 @@ app.use(express.json());
 app.use(cors());
 
 app.use(routes);
+
+//SCRAPE TEST (disable later)
+await scrapeKCNAWatch();
 
 //PORT to listen
 app.listen(CONFIG.vidPort);
