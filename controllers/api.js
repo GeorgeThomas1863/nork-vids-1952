f42cc5482d2ad465e4332317b2dd360ce9aab4a8
George Thomas <fake@email.org>
2025-07-12 18:50:35 -0400

    dev

--- controllers/api.js
+++ controllers/api.js
@@ -4,6 +4,10 @@ import { scrapeState } from "../src/state.js";
 
 export const apiRoute = async (req, res) => {
   const inputParams = req.body;
+
+  console.log("INPUT PARAMS");
+  console.log(inputParams);
+
   //updates the scrapeState
   const data = await runAdminCommand(inputParams);
   //return to displayer
