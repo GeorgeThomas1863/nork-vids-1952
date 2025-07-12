7b7762c6ef1c942529acbed5b5d184a604f3ed21
George Thomas <fake@email.org>
2025-07-12 18:44:31 -0400

    dev

--- src/scrape-control.js
+++ src/scrape-control.js
@@ -4,8 +4,25 @@ import { scrapeNewURLs } from "./urls.js";
 //!!! HERE
 //BUILD ADMIN COMMAND
 export const runAdminCommand = async (inputParams) => {
+  const { commandType } = inputParams;
+
   console.log("INPUT PARAMS");
   console.log(inputParams);
+
+  let data = {};
+  switch (commandType) {
+    case "admin-start-scrape":
+      data = await scrapeKCNAWatch();
+      break;
+    // case "admin-stop-scrape":
+    //   await scrapeKCNAWatch();
+    //   break;
+  }
+
+  console.log("DATA");
+  console.log(data);
+
+  return data;
 };
 
 export const scrapeKCNAWatch = async () => {
